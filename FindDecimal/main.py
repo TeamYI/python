d44c772469ec2bc85ad6d7eac4b4514429c3694e
deggru <staro_2@naver.com>
2021-02-15 23:29:33 +0900

    210215소수 찾기

--- FindDecimal/main.py
+++ FindDecimal/main.py
@@ -1,8 +1,19 @@
 # 소수 찾기 
 
-numberList = [1,2,3,4,5]
+numberList = [1,3,5,7]
 
-
+n = int(input())
+numberList = map(int, input().split())
+count = 0
 for i in numberList:
-    print(i)
-    print(numberList)
+    decimalCheck = True
+    for j in range(2,i):
+        if(i%j == 0):
+            decimalCheck = False
+            break
+
+    if decimalCheck == True and i != 1 :
+        count += 1
+
+    
+print(count)
